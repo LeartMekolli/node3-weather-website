e978fde5950e47427b4ec15ab381e8c6b2d96566
LeartMekolli <44322678+LeartMekolli@users.noreply.github.com>
2020-03-08 20:32:44 +0100

    Show apparent temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, lon, callback)=>{
         } else if(body.error){
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. It feels like '+body.currently.apparentTemperature+' degrees.')
         }
     })
 }
